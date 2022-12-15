5cd4f2efa64f291595fa50fcd2ad57cc53a218a4
Bruno Almeida <brunoalmeida069a@gmail.com>
2022-12-15 16:39:14 -0300

    Projeto completo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,25 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Realizar os testes com as classes Nokia e Iphone
+
+Iphone Celular1 = new Iphone("8429042", "G21", "Iphone14", 16);  
+
+Nokia Celular2 = new Nokia("45623922", "G21", "93228432", 16);
+
+Console.WriteLine("Iphone: ");
+
+Celular1.Ligar();
+
+Celular1.ReceberLigacao();
+
+Celular1.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("-------------------------------------");
+
+Console.WriteLine("Nokia: ");
+
+Celular2.Ligar();
+
+Celular2.ReceberLigacao();
+
+Celular2.InstalarAplicativo("Telegram");
